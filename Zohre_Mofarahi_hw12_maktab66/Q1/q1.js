a676060b7a6adeb546ade8f505a5b583cb82edc6
zohre <zohremofarahi@gmail.com>
2022-01-27 00:01:09 +0330

    exer12

---
+++ Zohre_Mofarahi_hw12_maktab66/Q1/q1.js
@@ -0,0 +1,11 @@
+function firstFunction() {
+    const finame=document.getElementById ("fname").value;
+    const laname=document.getElementById ("lname").value;
+   if (finame=="" || laname=="") {
+        alert ("please enter your name completely!")
+   }
+else
+ {
+    alert ("Your information was successfully registered!")
+}
+}
